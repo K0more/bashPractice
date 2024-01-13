dad0931eb83faa90a106d2c9bdfe712ef867ca07
K0more <arch.phenon@gmail.com>
2024-01-13 13:02:26 +0530

    feature added: calculate wages uptil now

--- employeewages.sh
+++ employeewages.sh
@@ -15,3 +15,22 @@ dailyWage=$((8*20))
 
 echo $dailyWage
 
+monthlyWage=$(($dailyWage*20))
+
+echo $monthlyWage
+
+workHours=0
+workDays=0
+wageEarned=0
+
+while [ $workHours -le 100 -a $workDays -le 20 ]
+do
+
+    if [ $isPresent -eq $checkAttendance ]
+    then
+        ((workHours+=8))
+        ((workDays++))
+        ((wageEarned+=160))
+fi
+        echo "current work hours $workHours current work days $workDays wage earned uptil now $wageEarned"
+done
